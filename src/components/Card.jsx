6221ce2fb2796887fd425556d35dd4f2da43eab0
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-11-28 17:00:51 +0100

    one single button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,9 +17,6 @@ export default function Card() {
             </p>
 
             <Button />
-            <button type="button" className="btn btn-warning rounded-1">
-              LEGGI DI PIÙ
-            </button>
           </div>
         </div>
       </div>
